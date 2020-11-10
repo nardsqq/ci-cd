7cdb5d590a2f522919b8395079530b04e4cc6c4a
nardsqq <nardparagas@gmail.com>
2020-11-10 23:55:33 +0800

    Test push

--- __tests__/pages/sum.test.js
+++ __tests__/pages/sum.test.js
@@ -1,5 +1,9 @@
 import { add } from "../../pages/sum"
 
 test('adds a and b', () => {
-  expect(add(1, 2)).toBe(4)
+  expect(add(1, 2)).toBe(3)
+})
+
+test('adds a and b', () => {
+  expect(add(2, 3)).toBe(3)
 })
